45e39b6113af72a0287479237d5aeff9e3738c4f
goshKon <124917537+goshKon@users.noreply.github.com>
2023-12-26 17:01:15 +0300

    launcher script
    check for model launcher script

---
+++ start.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+orange=$(cat/proc/cpuinfo | grep "model name" | awk '{print $7}' | head -n 1)	#Результат 5
+rasp=$(cat/proc/cpuinfo | grep "model name" | awk '{print $7}' | head -n 1)	#Результат 3
+if /cat/proc/cpuinfo | grep "model name" | awk '{print $7}' | head -n 1 == 5 
+then  
+	./orange.sh 
+else
+	./rasp.sh
+fi
