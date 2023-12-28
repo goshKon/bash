1d362124d338283fc97b0f927f1f71be3cde94b9
goshKon <124917537+goshKon@users.noreply.github.com>
2023-12-28 14:25:38 +0300

    Update rasp.sh
    fix syntax

--- rasp.sh
+++ rasp.sh
@@ -3,21 +3,14 @@
 stat_old=$(systemctl status isc-dhcp-server.service | awk '/Active/{print $2}')
 
 echo "Current status of isc-dhcp-server.service: $stat_old"
-
-if /sbin/ifconfig tun0 | grep -q "00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00" 
-then
-    echo "VPN is ALIVE FIRST RASP"
-elif 
-$rasp 
-then
-    if stat_old=active
+    if [ "stat_old"="active" ]
 	then
-        ping -c 5 172.81.0.1
-		if ($? -eq 0) 
+        pp=$(ping -c 5 172.81.0.1)
+		if [ $pp -eq 0 ] 
 		then
-            echo "VPN is ALIVE AFTER PING"
+            echo "Ping successful"
         else
-            echo "Restart VPN AFTER PING"
+            echo "Reboot VPN"
             killall -15 openvpn
             sleep 5
             /usr/sbin/openvpn --config /etc/openvpn/client.ovpn & >/dev/null 2>&1
@@ -25,7 +18,7 @@ then
 		fi   
 	fi
     elif 
-	stat_old=active||stat_old=inactive 
+	[ "stat_old"="active"||"stat_old"="inactive" ] 
 	then
         s3=$(systemctl start isc-dhcp-server.service)
         sleep 10
@@ -47,13 +40,15 @@ then
         /usr/sbin/openvpn --config /etc/openvpn/client.ovpn & >/dev/null 2>&1
         echo "FULL Restart VPN DHCP"
     
-fi
 # Добавлен код для проверки "Initialization Sequence Completed"
-while read -r line; do
-    echo "$line" 
-    if [[ "$line" == *"Initialization Sequence Completed"* ]]
-	then
+while read -r line
+ do
+    echo "$line"
+    if [[ $line==*"Initialization Sequence Completed"* ]]
+    then
         echo "Initialization Sequence Completed detected. Exiting the script."
-        exit 0
+		sleep 2
+        break
     fi
-done < <(/usr/sbin/openvpn --config /etc/openvpn/client.ovpn 2>&1)
+	done
+	exit 0
