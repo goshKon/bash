e2526119c33e843706db74bef92a69db7137bb02
goshKon <124917537+goshKon@users.noreply.github.com>
2023-12-28 12:52:44 +0300

    Update testrasp.sh
    fixing

--- testrasp.sh
+++ testrasp.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-rasp=$(cat/proc/cpuinfo | grep "model name" | awk '{print $7}' | head -n 1)#Результат 3
-if [ "rasp"=5 ] 
+rasp=$(cat /proc/cpuinfo | grep "model name" | awk '{print $7}' | head -n 1)#Результат 3
+if [ "rasp"="5" ] 
 then
   echo "orange: $rasp"
 	#etc/scripts/orange.sh 
 else
-  echo "rasp"
+  echo "rasp: $rasp"
 	#etc/scripts/rasp.sh
 fi
