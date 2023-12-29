3b58cd601a170cacb398bbc4ec42b5c6c67e509c
goshKon <124917537+goshKon@users.noreply.github.com>
2023-12-29 10:15:44 +0300

    Update rasp.sh
    fix syntax

--- rasp.sh
+++ rasp.sh
@@ -5,33 +5,33 @@ stat_old=$(systemctl status isc-dhcp-server.service | awk '/Active/{print $2}')
 
 echo "Current status of isc-dhcp-server.service: $stat_old"
 
-if /sbin/ifconfig tun0 | grep -q "00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00"
-then
-    echo "Initialization Sequence Completed"
-    elif
-    [ "$stat_old" = "active" ]
+
+if [ "$stat_old" = "active" ]
 	then
-        pp=$(ping -c 5 172.81.0.1)
-		if [ $pp -eq 0 ] 
+            ping -c 5 172.81.0.1 >/dev/null 2>&1
+		if [ $? -eq 0 ] 
 		then
             echo "Ping successful"
         else
-            echo "Reboot VPN"
-            killall -15 openvpn
+            echo "Rebooting VPN AFTER NO PING"
+            killall openvpn
             sleep 5
-            /usr/sbin/openvpn --config /etc/openvpn/client.ovpn & >/dev/null 2>&1
+          nohup /usr/sbin/openvpn --config /etc/openvpn/client.ovpn & >/dev/null 2>&1
 
 		fi   
 fi
+if /sbin/ifconfig tun0 | grep -q "00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00"
+then
+    echo "Initialization Sequence Completed"
     elif 
 	[ "$dhcl" != "$dhcl_com" ] 
 	then
  /sbin/ifconfig eth0 0.0.0.0 0.0.0.0 | dhclient & >/dev/null 2>&1
         sleep 10
-        killall -15 openvpn
+        killall openvpn
         sleep 5
-        /usr/sbin/openvpn --config /etc/openvpn/client.ovpn & >/dev/null 2>&1
-        echo "FULL Restart VPN DHCP"
+       nohup /usr/sbin/openvpn --config /etc/openvpn/client.ovpn & >/dev/null 2>&1 
+        echo "Restarting VPN DHCP"
 	else
         s3=$(systemctl start isc-dhcp-server.service)
         sleep 10
@@ -41,11 +41,11 @@ fi
         sleep 10
         echo "Restarting isc-dhcp-server.service: $s4"
         sleep 5
-        killall -15 openvpn
+        killall openvpn
         sleep 5
-        /usr/sbin/openvpn --config /etc/openvpn/client.ovpn & >/dev/null 2>&1
-        echo "FULL Restart VPN RASP"
-    
+       nohup /usr/sbin/openvpn --config /etc/openvpn/client.ovpn & >/dev/null 2>&1
+        echo "Restarting VPN fnsh"
+fi    
     
 # Добавлен код для проверки "Initialization Sequence Completed"
  if grep -q 'Initialization Sequence Completed' "$0"
