f4a52147b4855f14a24d2594c2a8676a2d114154
goshKon <124917537+goshKon@users.noreply.github.com>
2023-12-29 10:26:49 +0300

    Update orange.sh
    fix syntax

--- orange.sh
+++ orange.sh
@@ -5,19 +5,19 @@ stat_new=$(systemctl status dnsmasq.service | awk '/Active/{print $2}')
 
 echo "Current status of dnsmasq.service: $stat_new"
 
-if /sbin/ifconfig tun0 | grep -q "00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00"
+if [ "$dhcl" != "$dhcl_com" ] 
+	then
+ 			/sbin/ifconfig eth0 0.0.0.0 0.0.0.0 | dhclient & >/dev/null 2>&1 &
+			sleep 10
+			killall openvpn
+			sleep 5
+			nohup /usr/sbin/openvpn --config /etc/openvpn/client.ovpn & >/dev/null 2>&1 &
+			echo "Restarting DHCP"	
+   
+   if /sbin/ifconfig tun0 | grep -q "00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00"
 then
     echo "Initialization Sequence Completed"
-    elif
     
-[ "$dhcl" != "$dhcl_com" ] 
-	then
- 			/sbin/ifconfig eth0 0.0.0.0 0.0.0.0 | dhclient & >/dev/null 2>&1
-			sleep 10
-			killall -15 openvpn
-			sleep 5
-			/usr/sbin/openvpn --config /etc/openvpn/client.ovpn & >/dev/null 2>&1
-			echo "Restarting DHCP"	
    	else
         s1=$(systemctl start dnsmasq.service)
         sleep 10
@@ -27,14 +27,11 @@ then
         sleep 10
         echo "Restarting dnsmasq.service: $s2"
         sleep 5
-        killall -15 openvpn
+        killall openvpn
         sleep 5
-        /usr/sbin/openvpn --config /etc/openvpn/client.ovpn & >/dev/null 2>&1
-        echo "Restarting VPN"
-		
-
-			
-    
+        nohup /usr/sbin/openvpn --config /etc/openvpn/client.ovpn & >/dev/null 2>&1 &
+        echo "Restarting VPN"		
+ fi   
 fi
 # Добавлен код для проверки "Initialization Sequence Completed"
  if grep -q 'Initialization Sequence Completed' "$0"
